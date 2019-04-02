30f92a7f3a2db933cd1c6e26cae911d8854b6e27
William Pereira <william.pereira@me.com>
2019-04-02 10:30:51 -0300

    example 1 - hello world jcl

---
+++ ex-hello-world/genJCL.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const mustache = require('mustache')
+
+// -----------------------------------------------------------
+// Read config json and the template JCL
+// -----------------------------------------------------------
+
+const readConfig = () => {
+    try{
+        return JSON.parse(fs.readFileSync('config.json'));
+    } catch(e){
+        console.log('The data is empty...');
+        return [];
+    };
+};
+
+const readJCL = () => {
+    try{
+        return fs.readFileSync('hello-world.jcl').toString();
+    } catch(e){
+        console.log('The data is empty...');
+        return [];
+    };
+}
+
+let config = readConfig();
+let JCL = readJCL();
+
+// -----------------------------------------------------------
+// Use mustache to build the jcl to be submited
+// -----------------------------------------------------------
+
+let buildJCL = mustache.render(JCL,config)
+
+if (!fs.existsSync('./build')) fs.mkdirSync('./build');
+
+fs.writeFileSync('./build/custom.jcl', buildJCL);
+
+console.log('Generated custom JCL to ./build/custom.jcl');
--- ex-hello-world/hello-world.jcl
+++ ex-hello-world/hello-world.jcl
@@ -1,4 +1,4 @@
-//{{job.name}} JOB {{job.account}},'ASM/BIND/RUN',MSGCLASS=A,CLASS=A,
+//{{job.name}} JOB {{job.account}},'Alloc Example',MSGCLASS=A,CLASS=A,
 //             MSGLEVEL=(1,1),REGION=0M
 /*JOBPARM SYSAFF=*
 //*
@@ -6,5 +6,10 @@
 //* HELLO WORLD JCL
 //* *******************************************************************
 //*
-//HELLO  EXEC PGM=IEFBR14
-//DSNALL DD 
+//HELLO    EXEC PGM=IEFBR14
+//ALLOCDD  DD DSN={{dsn.hlq}},
+//       DISP=(NEW,CATLG), 
+//       DCB=(RECFM={{dsn.dcb.recordFormat}},
+//         BLKSIZE={{dsn.dcb.blockSize}},
+//         LRECL={{dsn.dcb.recordLength}}),      
+//       SPACE=(TRK,(1,1,1))
